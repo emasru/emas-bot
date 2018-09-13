7407dc8d247388bb10d9b5e23cf85f7da47a540e
emasru <henning.s.strandaa@gmail.com>
2018-09-13 11:09:04 +0200

    added iss command, gitignore
    -added iss command to bot with update imported from iss_tracker
    -added .pyc to gitignore

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import logging
 import time
 from datetime import datetime
-
+import update as f
 
 logging.basicConfig(level=logging.INFO)
 bot = commands.Bot(command_prefix='?', description="description")
@@ -28,8 +28,29 @@ async def marth():
     await bot.say("suger kuk")
 
 
+@bot.command(description="Gives the current position and address of the ISS")
+async def iss():
+    contents = f.pos_update()
+    if contents == 1:
+        await bot.say("Could not retrieve position")
+    position = contents.get("iss_position")
+    latitude = position.get("latitude")
+    longitude = position.get("longitude")
+    geo_name = f.location_query(position)
+    hemisphere = f.hemisphere_check(position)
+    message1 = "The current position of the ISS is Lat:", latitude, " and Lon:", longitude, "//", hemisphere
+    await bot.say(message1)
+    message2 = "Current address: ", geo_name
+    await bot.say(message2)  #TODO Fix reference error in message 1 and 2 (how to format strings?)
+
+
 @bot.command()
-async def add(left : int, right : int):
+async def roy():
+    await bot.say("is not our boy")
+
+
+@bot.command()
+async def add(left: int, right: int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
@@ -55,20 +76,20 @@ async def roll(dice: int):
 
 
 @bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
+async def choose(*choices: str):
     """Chooses between multiple choices."""
     await bot.say(random.choice(choices))
 
 
 @bot.command()
-async def repeat(times : int, content='repeating...'):
+async def repeat(times: int, content='repeating...'):
     """Repeats a message multiple times."""
     for i in range(times):
         await bot.say(content)
 
 
 @bot.command()
-async def joined(member : discord.Member):
+async def joined(member: discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
@@ -87,5 +108,5 @@ async def _bot():
     """Is the bot cool?"""
     await bot.say('Yes, the bot is cool.')
 
-bot.run(API_TOKEN)
 
+bot.run(API_TOKEN)
